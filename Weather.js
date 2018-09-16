f369ddf2c0344e611702d6eaf55701e2210b1636
L0GIC <cdhnet98@gmail.com>
2018-09-16 14:30:18 +0900

    날씨 추가 및 아이콘 변경

--- Weather.js
+++ Weather.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 
 const weatherCases = {
@@ -9,43 +9,49 @@ const weatherCases = {
     colors: ["#00C6FB", "#005BEA"],
     title: "짜증나게 비가 오네요",
     subtitle: "더 많은 정보를 알고싶으면 창 밖을 보세요",
-    icon: "ios-rainy"
+    icon: "weather-rainy"
   },
   Clear: {
     colors: ["#FEF253", "#FF7300"],
     title: "와우 맑아요",
     subtitle: "방에만 박혀있지 말고 좀 나가서 세상구경좀 하세요",
-    icon: "ios-sunny"
+    icon: "weather-sunny"
   },
   Thunderstorm: {
     colors: ["#00ECBC", "#007ADF"],
     title: "당신 집안에 천둥번개가 칩니다",
     subtitle: "음.. 사실 밖에서 쳐요",
-    icon: "ios-thunderstorm"
+    icon: "weather-lightning"
   },
   Clouds: {
     colors: ["#D7D2CC", "#304352"],
     title: "구름꼈네",
     subtitle: "더럽게 지루하죠?",
-    icon: "ios-cloudy"
+    icon: "weather-cloudy"
   },
   Snow: {
     colors: ["#7DE2FC", "#B9B6E5"],
     title: "와 눈온당",
     subtitle: "Do you want to build a snowman?",
-    icon: "ios-snow"
+    icon: "weather-snowy"
   },
   Drizzle: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "이슬비가 옵니다",
     subtitle: "근데 이슬비가 뭐지?",
-    icon: "ios-rainy-outside"
+    icon: "weather-hail"
   },
   Haze: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "악 안개꼈다",
     subtitle: "아무것도 안보여용",
-    icon: "ios-rainy"
+    icon: "weather-fog"
+  },
+  Mist: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist",
+    subtitle: "너가 안경 안낄때랑 비슷하네요",
+    icon: "weather-fog"
   }
 };
 
@@ -56,7 +62,7 @@ const Weather = ({ temp, weatherName }) => {
       style={styles.container}
     >
       <View style={styles.upper}>
-        <Ionicons
+        <MaterialCommunityIcons
           color="white"
           size={144}
           name={weatherCases[weatherName].icon}
